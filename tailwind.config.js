224fb75fb481c8ee5206a0c29e00e6be8329574f
Jose De La Cruz <jose.delcruz0490@icloud.com>
2024-09-01 21:06:23 -0400

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./*.{html,js}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				noto: ['Noto Sans', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
+};
